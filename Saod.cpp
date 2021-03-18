452a21e308776a79d0c7c39c0cde0948377840fe
Дмитрий Молько <molko_dmitriy@mail.ru>
2021-03-18 15:04:34 +0700

    Добавил 7ю лабу без мэйкфайла

---
+++ Saod.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <time.h>
+#include"Sourse.h"
+#include <time.h>
+int M, C;
+using namespace std;
+int main()
+{
+    srand(time(NULL));
+    int c;
+    int m;
+    int i;
+    cout << "n\tC 1version\tC 2version" << endl;
+
+    for (i = 100; i < 1001; i += 100)
+    {
+        int* a = new int[i];
+        int element;
+        cin >> element;
+        cout << i << "\t";
+        FillInc(a, i);
+        if (Bsearch1(a, i, element)) {
+            cout << C << "\t\t";
+        }
+        if (Bsearch2(a, i, element)) {
+            cout << C;
+        }
+        cout << endl;
+        delete[]a;
+    }
+    cout << endl;
+
+}
+
+
+
+
+
+
+
+
+
---
+++ Sourse.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <iostream>
+#include <time.h>
+#include <cmath>
+using namespace std;
+void FillInc(int*, int);
+void FillDec(int*, int);
+int CheckSum(int*, int);
+int RunNumber(int*, int);
+void PrintMas(int*, int);
+void FillRand(int*, int);
+void BubbleSort(int*, int);
+void ShakerSort(int*, int);
+void SelectSort(int*, int);
+void InsertSort(int* a, int n);
+void ShellSort(int* a, int n,bool);
+int knut(int l);
+int Sedfvig(int i);
+int count_of_step(int size, bool flag);
+int Bsearch1(int* a, int size, int element);
+int Bsearch2(int* a, int size, int element);
+extern int M, C;
