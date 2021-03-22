33cf4051f3bb08d54220a2a27b163e6014c2fcd7
Евгений <rabkov_evgeniy@mail.ru>
2021-03-22 16:31:57 +0700

    Ебать, добавил мэйк и поменял файловую структуру

---
+++ Makefile
@@ -0,0 +1,26 @@
+CC = g++
+CFlags = -Wall -MMD -I src
+PObj = obj/
+PSrc = src/
+PBin = bin/
+EXEC= out
+Target = $(PBin)main7.$(EXEC)
+
+all: $(Target)
+
+$(PBin)%.$(EXEC):$(PObj)%.o $(PObj)SAOD.a
+	$(CC) $(CFlags) -o $@ $^
+
+$(PObj)SAOD.a: $(PObj)SAOD.o
+	ar rcs $@ $^
+
+$(PObj)%.o:$(PSrc)%.cpp
+	$(CC) -c $^ $(CFlags) -o $@
+
+.PHONY: clean all
+
+clean:
+	rm -rf $(PObj)*.*
+	rm -rf $(PBin)*.*
+
+-include $(Pobj)SAOD.d
---
+++ src/main7.cpp
@@ -0,0 +1,48 @@
+#include <SAOD.h>
+
+int M, C;
+using namespace std;
+int main() {
+  srand(time(NULL));
+  int i;
+
+  cout << "n\tC(first) 1version\tC(mid)\tC(last)\t\tC(first) "
+          "2version\tC(mid)\tC(last)\t\t1 not\t2not"
+       << endl;
+  for (i = 100; i < 1001; i += 100) {
+    int *a = new int[i];
+    int first = 1;
+    int mid = i / 2;
+    int last = i;
+    cout << i << "\t";
+    FillInc(a, i);
+    cout << Bsearch1(a, i, first) << "\t\t\t";
+    cout << Bsearch1(a, i, mid) << "\t";
+    cout << Bsearch1(a, i, last) << "\t\t";
+    cout << Bsearch2(a, i, first) << "\t\t\t";
+    cout << Bsearch2(a, i, mid) << "\t";
+    cout << Bsearch2(a, i, last) << "\t\t";
+    cout << Bsearch1(a, i, i + 1) << "\t";
+    cout << Bsearch2(a, i, i + 1);
+    cout << endl;
+    Bsearch1(a, i, first);
+    cout << '\t' << C << "\t\t\t";
+    Bsearch1(a, i, mid);
+    cout << C << "\t";
+    Bsearch1(a, i, last);
+    cout << C << "\t\t";
+    Bsearch2(a, i, first);
+    cout << C << "\t\t\t";
+    Bsearch2(a, i, mid);
+    cout << C << "\t";
+    Bsearch2(a, i, last);
+    cout << C << "\t\t";
+    Bsearch1(a, i, i + 1);
+    cout << C << "\t";
+    Bsearch2(a, i, i + 1);
+    cout << C;
+    cout << endl << endl;
+    delete[] a;
+  }
+  cout << endl;
+}
