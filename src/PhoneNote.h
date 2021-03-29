08ae35389e0ef963700d14e6e9b76ad10097718c
Евгений <rabkov_evgeniy@mail.ru>
2021-03-30 03:43:32 +0700

    Вынес класс в отдельный файл

---
+++ src/PhoneNote.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+
+class PhoneNote {
+public:
+  enum KeysForSort { Number, Name, Age, Organizations };
+
+  PhoneNote(string number, string name, int age, string organizations);
+
+  void edit_phone_note(string number, string name, int age,
+                       string organizations);
+
+  string get_number();
+  string get_name();
+  int get_age();
+  string get_organizations();
+
+  static void set_key_for_sort(KeysForSort key);
+
+  bool operator<(&PhoneNote note);
+
+private:
+  static KeysForSort key_for_sort;
+  string number;
+  string name;
+  int age;
+  string organizations;
+};
+
+ostream &operator<<(ostream &os, const PhoneNote &note);
--- src/SAOD.h
+++ src/SAOD.h
@@ -24,31 +24,3 @@ int Sedfvig(int i);
 int count_of_step(int size, bool flag);
 int Bsearch1(int *a, int size, int element);
 int Bsearch2(int *a, int size, int element);
-
-class PhoneNote {
-public:
-  enum KeysForSort { Number, Name, Age, Organizations };
-
-  PhoneNote(string number, string name, int age, string organizations);
-
-  void edit_phone_note(string number, string name, int age,
-                       string organizations);
-
-  string get_number();
-  string get_name();
-  int get_age();
-  string get_organizations();
-
-  static void set_key_for_sort(KeysForSort key);
-
-  bool operator<(&PhoneNote note);
-
-private:
-  static KeysForSort key_for_sort;
-  string number;
-  string name;
-  int age;
-  string organizations;
-};
-
-ostream &operator<<(ostream &os, const PhoneNote &note);
